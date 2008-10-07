deaaf6c80f84ce9f1402f418b97aaff3d80a153d
Charles Melbye <charlie@mudoo.net>
2008-10-06 20:23:20 -0500

    Fixed a lot of code

--- bot.rb
+++ bot.rb
@@ -1,9 +1,18 @@
 $LOAD_PATH << './lib'
+puts "Loading IRC..."
 require 'irc'
+puts "Loading the IRC Parser..."
 require 'parser'
+puts "Loading RubyGems..."
 require 'rubygems'
+puts "Loading Activerecord..."
 require 'activerecord'
+puts "Loading models and connecting to database..."
 require 'models'
+puts "Loading HPricot, OpenURI and ERB..."
+require 'hpricot'
+require 'open-uri'
+require 'erb'
 
 $b = binding()
 
@@ -19,7 +28,7 @@ irc = IRC.new( :server => 'irc.freenode.org',
 parser = Parser.new
 
 irc.on_001 do
-	irc.join '#botters'
+	irc.join '##gpt'
 end
 
 irc.on_all_events do |e|
@@ -30,25 +39,33 @@ irc.on_invite do |e|
   irc.join(e.channel)
 end
 
-irc.on_join do |e|
-  irc.msg(e.channel, "Hey #{e.sender.nick}, and welcome to #{e.channel}!") if e.sender.nick != nick
-end
+# Uncomment this block if you want it to greet people
+#irc.on_join do |e|
+#  irc.msg(e.channel, "Hey #{e.sender.nick}, and welcome to #{e.channel}!") if e.sender.nick != nick
+#end
 
 irc.on_privmsg do |e|
-  parser.command(e, 'whatis') do |c, params|
-    factoid = Factoid.find_by_key(c.message)
-    if factoid
-      irc.msg(e.recipient, "#{factoid.key} is #{factoid.value} [added by #{factoid.creator}]")
-    else
-      irc.msg(e.recipient, "Unable to find the #{factoid.key} factoid!")
-    end
-  end
   
   parser.command(e, 'eval', true) do |c, params|
     begin
       irc.msg(e.recipient, eval(c.message, $b, 'eval', 1))
     rescue Exception => error
       irc.msg(e.recipient, 'compile error')
+    end
+  end
+  
+  parser.command(e, 'join', true) do |c, params|
+    irc.join(c.message)
+  end
+  
+  parser.command(e, 'calc') do |c, params|
+    url = "http://www.google.com/search?q=#{ERB::Util.u(c.message)}"
+    doc = Hpricot(open(url))
+    calculation = (doc/'/html/body//#res/p/table/tr/td[3]/h2/font/b').inner_html
+    if calculation.empty?
+      irc.msg(e.recipient, 'Invalid Calculation.')
+    else
+      irc.msg(e.recipient, calculation)
     end
   end
   
@@ -66,8 +83,16 @@ irc.on_privmsg do |e|
     if factoid
       irc.msg(e.recipient, "#{factoid.key} is #{factoid.value} [added by #{factoid.creator}]")
     else
-      irc.msg(e.recipient, "Unable to find the #{factoid.key} factoid!")
+      irc.msg(e.recipient, "Unable to find the #{$1} factoid!")
     end
+  end
+  
+  if e.message =~ /^\001ACTION (hugs|licks|kisses|huggles|snuggles up with|loves) #{nick}(.*?)\001$/
+    irc.msg(e.recipient, 'Aww :-)')
+  end
+  
+  if e.message =~ /^\001ACTION (kills|farts on|eats|drinks|poops on|sets fire to) #{nick}(.*?)\001$/
+    irc.msg(e.recipient, "You're mean :-(")
   end
 end
 
